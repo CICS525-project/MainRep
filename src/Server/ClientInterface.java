70249b237d998868ce85c4a8393d617b8247b861
Ifeanyi Agu <yankitwizzy@gmail.com>
2014-07-25 15:30:26 -0700

    added RMI server connection params

---
+++ src/Server/ClientInterface.java
@@ -0,0 +1,14 @@
+package Server;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author welcome
+ */
+public interface ClientInterface extends Remote {
+	//public String name;
+	
+    public double findScore() throws RemoteException;
+    
+}
--- src/Server/ServerInterface.java
+++ src/Server/ServerInterface.java
@@ -1,27 +1,14 @@
 package Server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-import BlobManager.BlobProperties;
-import Global.User;
+import java.rmi.*;
 
 /**
  *
  * @author welcome
  */
 public interface ServerInterface extends Remote {
+    public double findScore(String name) throws RemoteException;
     
-	public boolean authenticateUser(User u);
-	
-	public void blobAdded(BlobProperties b);
-	
-	public void blobDeleted(BlobProperties b);
-	
-	public void replicateBlobChange(String command, BlobProperties b);
-	
-	public double findScore(String name) throws RemoteException;    
-   
+    public String echoIP(ClientInterface client) throws RemoteException;
     
     public String copyBlob() throws RemoteException;
     
